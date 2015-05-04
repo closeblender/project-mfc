9014a2b3b5691b8472d7e5d7129032864e560283
Trevor <tdp321@live.com>
2015-05-04 13:40:40 -0500

    cursor

---
+++ Assets/Scripts/crossHairs.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class crossHairs : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Screen.showCursor = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		pos.z = 0;
+		transform.position = pos;
+
+	}
+}
